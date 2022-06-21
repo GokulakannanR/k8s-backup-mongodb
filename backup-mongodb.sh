8b9d50734f280c33bd7ad855fd7bfaf9d1478eac
GokulakannanR <101801425+GokulakannanR@users.noreply.github.com>
2022-06-21 10:40:54 +0530

    Update backup-mongodb.sh

--- backup-mongodb.sh
+++ backup-mongodb.sh
@@ -11,27 +11,13 @@ if [ -n "$RedisOplog" ]; then
 fi
 
 echo "[$SCRIPT_NAME] Dumping all RedisDB databases to compressed archive..."
-
-redisdump $OPLOG_FLAG \
-	--archive="$ARCHIVE_NAME" \
-	--gzip \
-	--uri "$RedisURI"
-
-COPY_NAME=$ARCHIVE_NAME
-if [ ! -z "$PASSWORD_7ZIP" ]; then
-    echo "[$SCRIPT_NAME] 7Zipping with password..."
-    COPY_NAME=redisdump_$(date +%Y%m%d_%H%M%S).7z
-    7za a -tzip -p"$PASSWORD_7ZIP" -mem=AES256 "$COPY_NAME" "$ARCHIVE_NAME"
-fi
-
-
 S3_ENDPOINT_OPT=""
 if [ ! -z "$S3_ENDPOINT_URL" ]; then
   S3_ENDPOINT_OPT="--endpoint-url $S3_ENDPOINT_URL"
 fi
 
 echo "[$SCRIPT_NAME] Uploading compressed archive to S3 bucket..."
-aws ${S3_ENDPOINT_OPT} s3 cp "$COPY_NAME" "$BUCKET_URI/$COPY_NAME"
+aws ${S3_ENDPOINT_OPT} s3 cp "/data/dump.rdb" "$BUCKET_URI/redisdump_$(date +%Y%m%d_%H%M%S).rdb"
 
 echo "[$SCRIPT_NAME] Cleaning up compressed archive..."
 rm "$COPY_NAME"
